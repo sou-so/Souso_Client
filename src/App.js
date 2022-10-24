99fa044c0a0a84660efe6b40b06c1d3e2107f447
Joy Lee <joylee.dev@gmail.com>
2022-10-24 21:09:19 +0900

    💄 add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { JoinPage, LoginPage, MainPage } from 'pages';
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/GlobalStyles';
+import theme from 'styles/Theme';
 
 function App() {
   return (
-    <div className="container">
-      <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="join" element={<JoinPage />} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="container">
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="join" element={<JoinPage />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/Theme.js
@@ -0,0 +1,27 @@
+const color = {
+  main: '#178A5F',
+  black_1: '#333',
+  white_1: '#FFF',
+  gray_1: '#EEE'
+};
+
+const style = {
+  border_dark: '1px solid #9A9A9A',
+  border_light: '1px solid #EAEAEA',
+  edge_padding: '12px'
+};
+
+const font = {
+  xs: '0.5rem',
+  sm: '0.75rem',
+  md: '1.25rem',
+  lg: '1.5rem'
+};
+
+const theme = {
+  color,
+  style,
+  font
+};
+
+export default theme;
