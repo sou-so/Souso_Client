0a04ad5ccb54ecf3b6df32c9c5181bc54fe76a15
Joy Lee <joylee.dev@gmail.com>
2022-10-24 19:44:31 +0900

    ♻ change extensions js to jsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
-export { JoinPage } from './JoinPage'
-export { LoginPage } from './LoginPage';
-export { MainPage } from './MainPage';
+export { JoinPage } from './JoinPage/JoinPage';
+export { LoginPage } from './LoginPage/LoginPage';
+export { MainPage } from './MainPage/MainPage';
