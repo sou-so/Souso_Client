cbf5976fd97146c4f090f2e98a52cff02a359759
Joy Lee <joylee.dev@gmail.com>
2022-10-24 15:12:06 +0900

    💄 GlobalStyles 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { MainPage } from 'pages/MainPage';
+import GlobalStyles from 'styles/GlobalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <MainPage />
     </div>
   );
