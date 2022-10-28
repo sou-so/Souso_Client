956e021c087bfad54b695f9199079053d9533720
Joy Lee <joylee.dev@gmail.com>
2022-10-28 19:22:52 +0900

    ✨ get current Geolocation in TownAuth page

--- src/pages/TownAuthPage/TownAuthPage.jsx
+++ src/pages/TownAuthPage/TownAuthPage.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageHeader } from 'components/Common';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import * as S from './styles';
 
 export const TownAuthPage = () => {
+  const [geo, setGeo] = useState({ lat: 33.450701, lng: 126.570667 });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(position => {
+        setGeo({
+          lat: position.coords.latitude, // 위도
+          lng: position.coords.longitude // 경도
+        });
+      });
+    }
+  }, []);
+
   return (
     <>
       <PageHeader backTo="/" title="동네 인증" />
       <S.PageContainer>
-        <Map center={{ lat: 33.5563, lng: 126.79581 }} className="kakao_map">
-          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }} />
+        <Map center={{ lat: geo.lat, lng: geo.lng }} className="kakao_map">
+          <MapMarker position={{ lat: geo.lat, lng: geo.lng }} />
         </Map>
       </S.PageContainer>
     </>
