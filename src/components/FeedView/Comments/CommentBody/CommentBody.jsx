4f28a0dda6d093e4e21111e8f25f6bcbabfdc2cf
JeongJun <rgfdds98@gmail.com>
2022-11-16 18:52:22 +0900

    💄 styling CommentBody

--- src/components/FeedView/Comments/CommentBody/CommentBody.jsx
+++ src/components/FeedView/Comments/CommentBody/CommentBody.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Icon, ProfileImage } from 'components/Common';
+import { ReactComponent as comment } from 'assets/icons/comment.svg';
 import * as S from './styles';
 
 export const CommentBody = ({ data }) => {
+  const [commentUser, setCommentUser] = useState('');
+
   return (
     <S.CommentContainer>
-      <S.CommentNumber></S.CommentNumber>
-      <S.CommentWrap></S.CommentWrap>
+      <S.CommentCount>댓글 ({data.comments})</S.CommentCount>
+      <S.CommentWrap>
+        <S.OriginComment>
+          <div className="comment_user">
+            <ProfileImage size={40} url={commentUser} />
+            <p className="nickname">토끼 세마리</p>
+          </div>
+
+          <div className="comment_text">
+            카페 찾고있었는데 좋네요! 조만간 한 번 가봐야겠어요~ 추천 디저트
+            있나요?
+          </div>
+
+          <div className="comment_footer">
+            <button>
+              <Icon Icon={comment} size={17} />
+              <span>답글쓰기</span>
+            </button>
+
+            <p>12분 전</p>
+          </div>
+        </S.OriginComment>
+      </S.CommentWrap>
     </S.CommentContainer>
   );
 };
--- src/components/FeedView/Comments/CommentBody/styles.js
+++ src/components/FeedView/Comments/CommentBody/styles.js
@@ -1,9 +1,53 @@
 import styled from 'styled-components';
 
-export const CommentContainer = styled.div`
-  position: relative;
+export const CommentContainer = styled.div``;
+
+export const CommentCount = styled.div`
+  font-size: 15px;
+  padding: 0 0 12px 10px;
 `;
 
-export const CommentNumber = styled.div``;
-
 export const CommentWrap = styled.div``;
+
+export const OriginComment = styled.div`
+  .comment_user {
+    display: flex;
+    align-items: center;
+    padding-bottom: 9px;
+    .nickname {
+      font-size: 13px;
+      font-weight: 500;
+      line-height: 28px;
+      padding-left: 5px;
+    }
+  }
+  .comment_text {
+    padding: 0 10px;
+    font-size: 15px;
+  }
+  .comment_footer {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 10px 0 12px;
+    button {
+      display: flex;
+      align-items: center;
+      path {
+        stroke: ${({ theme }) => theme.color.black_2};
+      }
+      span {
+        font-size: 13px;
+        line-height: 19.5px;
+        padding-left: 4.5px;
+        color: ${({ theme }) => theme.color.black_2};
+      }
+    }
+    p {
+      font-size: 11px;
+      color: ${({ theme }) => theme.color.black_2};
+      line-height: 28px;
+    }
+  }
+`;
+
+export const Recomment = styled.div``;
