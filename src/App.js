3f7ec6397aefe8a806d2402d44ff31c7cec31c34
Joy Lee <joylee.dev@gmail.com>
2022-11-22 04:53:39 +0900

    ✨ redirect pages if token exists

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { AppLayout } from 'components';
+import { AppLayout, AuthLayout } from 'components';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/GlobalStyles';
 import theme from 'styles/Theme';
@@ -11,8 +12,14 @@ function App() {
       <GlobalStyles />
       <div className="container">
         <Routes>
+          {/* No Token */}
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<P.LoginPage />} />
+            <Route path="join" element={<P.JoinPage />} />
+          </Route>
+
           {/* Navbar included */}
-          <Route path="/" element={<AppLayout />}>
+          <Route path="/" element={<AppLayout navbar />}>
             <Route index element={<P.FeedPage />} />
             <Route path="meetup" element={<P.MeetPage />} />
             <Route path="chats" element={<P.ChatPage />} />
@@ -20,14 +27,13 @@ function App() {
           </Route>
 
           {/* Navbar not included */}
-          <Route path="login" element={<P.LoginPage />} />
-          <Route path="join" element={<P.JoinPage />} />
+          <Route path="/" element={<AppLayout />}>
+            <Route path="feed/:id" element={<P.FeedViewPage />} />
+            <Route path="feed/add" element={<P.FeedAddPage />} />
+            <Route path="mypage/posts" element={<P.MyPostPage />} />
+            <Route path="mypage/scraps" element={<P.MyScrapPage />} />
+          </Route>
           <Route path="mytown" element={<P.TownAuthPage />} />
-
-          <Route path="feed/:id" element={<P.FeedViewPage />} />
-          <Route path="feed/add" element={<P.FeedAddPage />} />
-          <Route path="mypage/posts" element={<P.MyPostPage />} />
-          <Route path="mypage/scraps" element={<P.MyScrapPage />} />
         </Routes>
       </div>
     </ThemeProvider>
