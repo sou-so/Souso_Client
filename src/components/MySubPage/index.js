8bd64ce1b7813eae021ec611c8b63f8dda8ba3c4
Joy Lee <joylee.dev@gmail.com>
2022-12-01 02:25:55 +0900

    ✨ request my bookmark list API filtering by category

--- src/components/MySubPage/index.js
+++ src/components/MySubPage/index.js
@@ -3,3 +3,4 @@ export { EditDeleteButton } from './EditDeleteButton/EditDeleteButton';
 export { MyPosts } from './MyPosts/MyPosts';
 export { MyMeets } from './MyMeets/MyMeets';
 export { MyComments } from './MyComments/MyComments';
+export { CategoryTab } from './CategoryTab/CategoryTab';
